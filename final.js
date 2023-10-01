efbc8f619e18140459a19161701f7e7c095b0ae1
Matsvei Khmialnitski <geniusx1990@gmail.com>
2023-10-01 14:54:23 +0300

    fix: solved  without recursion

--- final.js
+++ final.js
@@ -1,39 +1,50 @@
-const json = '{"name": "John", "age": 30, "city": "New York", "colors": ["red", 22, "blue"], "contacts": {"email": "john@gmail.com", "phone": "1231231233"}}';
+const json = '{"name": "John", "age": 30, "city": "New York", "married": true, "graduated": false, "childrens": ["Mike", 15, "Ivan", 22], "father": {"name": "Oleg", "age": 80}}';
 
 function myJSONParse(string) {
-    let reg = /"([^"]+)":\s*("[^"]*"|true|false|null|\d+(\.\d+)?|\{[^{}]*\}|\[[^\[\]]*\])/g;
-    let result = {};
+    const reg = /"(?<key>[^"]+)":\s*(?:"(?<value>[^"]+)|(?<number>\d+)|(?<boolean>true|false)|(?<null>null)|\[(?<array>[^\]]*)\]|\{(?<objectNested>[^}]+)\})/g;
+
+    const result = {};
+
     let section = result;
+
     let match;
 
     while ((match = reg.exec(string)) !== null) {
-        let key = match[1];
-        let value = match[2];
+        let key = match.groups.key;
+        let value = match.groups.value;
 
-        if (value === 'true') {
-            value = true;
-        } else if (value === 'false') {
-            value = false;
-        } else if (value === 'null') {
+        if (match.groups.number !== undefined) {
+            value = parseFloat(match.groups.number);
+        } else if (match.groups.boolean !== undefined) {
+            value = match.groups.boolean === 'true';
+        } else if (match.groups.null !== undefined) {
             value = null;
-        } else if (!isNaN(value)) {
-            value = parseFloat(value);
-        } else if (value.startsWith('"') && value.endsWith('"')) {
-            value = value.slice(1, -1);
-        } else if (value.startsWith('{') && value.endsWith('}')) {
-            value = myJSONParse(value);
-        } else if (value.startsWith('[') && value.endsWith(']')) {
-            const arrayContent = value.slice(1, -1);
-            value = arrayContent.split(',').map(item => {
+        } else if (match.groups.array !== undefined) {
+            value = match.groups.array.split(',').map(item => {
                 item = item.trim();
                 if (!isNaN(item)) {
                     return parseFloat(item);
-                } else {
-                    return item.replace(/"/g, '');
                 }
+                return item.replace(/"/g, '');
             });
-        } else {
-            value = value.replace(/"/g, '');
+        } else if (match.groups.objectNested !== undefined) {
+            const nestedObjectString = match.groups.objectNested;
+            const jsonObject = {};
+            const regObj = /"(?<key>[^"]+)":\s*(?:"(?<value>[^"]+)"|(?<number>\d+))/g;
+            let test;
+
+            while ((test = regObj.exec(nestedObjectString)) !== null) {
+                const key = test.groups.key;
+                const value = test.groups.value;
+
+                if (test.groups.number !== undefined) {
+                    jsonObject[key] = parseFloat(test.groups.number);
+                } else {
+                    jsonObject[key] = value;
+                }
+            }
+
+            value = jsonObject;
         }
 
         section[key] = value;
@@ -42,4 +53,4 @@ function myJSONParse(string) {
     return result;
 }
 
-console.log(myJSONParse(json));
+console.log(myJSONParse(json))
